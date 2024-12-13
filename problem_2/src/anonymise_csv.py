8f657ba1639b014132ff1473f8a630f043d889c3
asdfblarg <david82794@yahoo.com>
2024-12-13 15:23:04 -0500

    split up dask read and write into separate modular functions

--- problem_2/src/anonymise_csv.py
+++ problem_2/src/anonymise_csv.py
@@ -10,20 +10,29 @@ def hash_value(value) -> str:
     return hashlib.sha256(str(value).encode("utf-8")).hexdigest()
 
 
+def read_input_csv(input_file: str):
+    """Read in input csv"""
+    return dd.read_csv(input_file)
+
+
+def write_output_csv(output_file: str):
+    """Read in input csv"""
+    df.to_csv(output_file, index=False, single_file=True)
+
+
 def anonymize_with_hashing(input_file: str, output_file: str, cols: list[str]):
-    df = dd.read_csv(input_file)
+    """Read input csv, anonymize columns, write to output csv"""
+    df = read_input_csv(input_file)
 
     # Hash each column to anonymize
     for col in cols:
         df[col] = df[col].map_partitions(lambda partition: partition.apply(hash_value))
 
-    df.to_csv(output_file, index=False, single_file=True)
+    write_output_csv(output_file)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Anonymizes csv columns with hashing"
-    )
+    parser = argparse.ArgumentParser(description="Anonymizes csv columns with hashing")
     parser.add_argument("-i", "--input", help="Input csv filename", required=True)
     parser.add_argument("-o", "--output", help="Output csv filename", required=True)
     args = parser.parse_args()
